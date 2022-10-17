4829855d0a45209f4c991ea20854baa8caf5df71
Pedro Fotninele <pedro.p.h.fontinele@icloud.com>
2022-10-17 20:05:57 -0300

    Primeiro commit

---
+++ 02. Back-end/src/MyEvents.API/Controllers/v1/Evento/EventoController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyEvents.API.Controllers.v1
+{
+    [ApiController]
+    [ApiVersion("1")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Retonar Get";
+        }
+
+        [HttpGet("{id}")]
+        public string Get(uint id)
+        {
+            return "Retonar Get por id";
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "Exemplo de Post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(uint id)
+        {
+            return "Exemplo de Put";
+        }
+
+        [HttpDelete]
+        public string Delete()
+        {
+            return "Exemplo de Delete";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(uint id)
+        {
+            return "Exemplo de Delete por id";
+        }
+    }
+}
---
+++ 02. Back-end/src/MyEvents.API/Models/Evento/Evento.cs
@@ -0,0 +1,13 @@
+﻿namespace MyEvents.API.Models
+{
+    public class Evento
+    {
+        public uint IdEvento { get; set; }
+        public string Local { get; set; }
+        public string DataEvento { get; set; }
+        public string Tema { get; set; }
+        public int QtdParticipantes { get; set; }
+        public string Lote { get; set; }
+        public string ImagemUrl { get; set; }
+    }
+}
